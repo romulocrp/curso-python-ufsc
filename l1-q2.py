3c3a86722d3ec489be61f0a8c43b7334aca0c10b
romulo <romulolespaul@gmail.com>
2022-08-23 14:41:38 -0300

    question 2 of the first list solved

---
+++ l1-q2.py
@@ -0,0 +1,6 @@
+n1, n2 = [int(x) for x in input("Insira os números separados por vírgula: ").split(", ")]
+summation = n1 + n2
+subtraction = n1 - n2
+product = n1 * n2
+division = n1 / n2
+print(f"O valor final das operações são: soma = {summation}, subtração = {subtraction}, produto = {product}, divisão = {division}")
